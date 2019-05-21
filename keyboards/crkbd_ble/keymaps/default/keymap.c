a1d746f0f3c3b2efe6f5e12d70d41196e0e0051a
forestsource <forestsources@gmai.com>
2019-05-21 23:57:53 +0900

    wip

---
+++ keyboards/crkbd_ble/keymaps/default/keymap.c
@@ -0,0 +1,241 @@
+#include QMK_KEYBOARD_H
+#include "app_ble_func.h"
+
+#include "crkbd.h"
+// #include "action_layer.h"
+// #include "action_util.h"
+// #include "eeconfig.h"
+// #ifdef PROTOCOL_LUFA
+// #include "lufa.h"
+// #include "split_util.h"
+// #endif
+// #include "LUFA/Drivers/Peripheral/TWI.h"
+// #ifdef SSD1306OLED
+//   #include "ssd1306.h"
+// #endif
+
+// #include "../lib/mode_icon_reader.c"
+// #include "../lib/layer_state_reader.c"
+// #include "../lib/host_led_state_reader.c"
+// #include "../lib/logo_reader.c"
+// #include "../lib/keylogger.c"
+// #include "../lib/timelogger.c"
+
+extern keymap_config_t keymap_config;
+
+// #ifdef RGBLIGHT_ENABLE
+// //Following line allows macro to read current RGB settings
+// extern rgblight_config_t rgblight_config;
+// #endif
+
+// extern uint8_t is_master;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _LOWER 3
+#define _RAISE 4
+#define _BLE 5
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+  BLE,
+  BACKLIT,
+  RGBRST,
+
+  AD_WO_L = SAFE_RANGE, /* Start advertising without whitelist  */
+  BLE_DIS,              /* Disable BLE HID sending              */
+  BLE_EN,               /* Enable BLE HID sending               */
+  USB_DIS,              /* Disable USB HID sending              */
+  USB_EN,               /* Enable USB HID sending               */
+  DELBNDS,              /* Delete all bonding                   */
+  ADV_ID0,              /* Start advertising to PeerID 0        */
+  ADV_ID1,              /* Start advertising to PeerID 1        */
+  ADV_ID2,              /* Start advertising to PeerID 2        */
+  ADV_ID3,              /* Start advertising to PeerID 3        */
+  ADV_ID4,              /* Start advertising to PeerID 4        */
+  BATT_LV,              /* Display battery level in milli volts */
+  DEL_ID0,              /* Delete bonding of PeerID 0           */
+  DEL_ID1,              /* Delete bonding of PeerID 1           */
+  DEL_ID2,              /* Delete bonding of PeerID 2           */
+  DEL_ID3,              /* Delete bonding of PeerID 3           */
+  DEL_ID4,              /* Delete bonding of PeerID 4           */
+  ENT_DFU,              /* Start bootloader                     */
+  ENT_SLP,              /* Deep sleep mode                      */
+};
+
+enum macro_keycodes {
+  KC_SAMPLEMACRO,
+};
+
+#define KC______ KC_TRNS
+#define KC_XXXXX KC_NO
+#define KC_LOWER LOWER
+#define KC_RAISE RAISE
+#define KC_BLE   BLE
+#define KC_RST   RESET
+#define KC_LRST  RGBRST
+#define KC_LTOG  RGB_TOG
+#define KC_LHUI  RGB_HUI
+#define KC_LHUD  RGB_HUD
+#define KC_LSAI  RGB_SAI
+#define KC_LSAD  RGB_SAD
+#define KC_LVAI  RGB_VAI
+#define KC_LVAD  RGB_VAD
+#define KC_LSMOD RGB_SMOD
+#define KC_CTLTB CTL_T(KC_TAB)
+#define KC_GUIEI GUI_T(KC_LANG2)
+#define KC_ALTKN ALT_T(KC_LANG1)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        ESC,     Q,     W,     E,     R,     T,                      Y,     U,     I,     O,     P,  BSPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      CTLTB,     A,     S,     D,     F,     G,                      H,     J,     K,     L,  SCLN,  QUOT,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       LSFT,     Z,     X,     C,     V,     B,                      N,     M,  COMM,   DOT,  SLSH,  RSFT,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_LOWER] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        ESC,     1,     2,     3,     4,     5,                      6,     7,     8,     9,     0,  BSPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      CTLTB,    F1,    F2,    F3,    F4,    F5,                     F6,    F7,    F8,    F9,   F10, XXXXX,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       LSFT,   F11,   F12,   F13,   F14,   F15,                    F16,   F17,   F18,   F19,   F20, XXXXX,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
+                              //`--------------------'  `--------------------'
+  ),
+
+/*
+  [_RAISE] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        ESC,  EXLM,    AT,  HASH,   DLR,  PERC,                   CIRC,  AMPR,  ASTR,  LPRN,  RPRN,  BSPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      CTLTB, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                   MINS,   EQL,  LCBR,  RCBR,  PIPE,   GRV,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       LSFT, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                   UNDS,  PLUS,  LBRC,  RBRC,  BSLS,  TILD,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
+                              //`--------------------'  `--------------------'
+  ),
+  */
+
+  [_ADJUST] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        RST,  LRST, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       LTOG,  LHUI,  LSAI,  LVAI, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      LSMOD,  LHUD,  LSAD,  LVAD, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
+                              //`--------------------'  `--------------------'
+  )
+  /*
+
+  [_BLE] = LAYOUT( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+     XXXXX,  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+     XXXXX,  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+     XXXXX,  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                  KC_GUIEI, LOWER,   KC_SPC,      KC_ENT, RAISE, ALTKN \
+                              //`--------------------'  `--------------------'
+  )*/
+};
+
+int RGB_current_mode;
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  char str[16];
+  if (record->event.pressed) {
+    switch (keycode) {
+    case DELBNDS:
+      delete_bonds();
+      return false;
+    case AD_WO_L:
+      restart_advertising_wo_whitelist();
+      return false;
+    case USB_EN:
+      set_usb_enabled(true);
+      return false;
+      break;
+    case USB_DIS:
+      set_usb_enabled(false);
+      return false;
+      break;
+    case BLE_EN:
+      set_ble_enabled(true);
+      return false;
+      break;
+    case BLE_DIS:
+      set_ble_enabled(false);
+      return false;
+      break;
+    case ADV_ID0:
+      restart_advertising_id(0);
+      return false;
+    case ADV_ID1:
+      restart_advertising_id(1);
+      return false;
+    case ADV_ID2:
+      restart_advertising_id(2);
+      return false;
+    case ADV_ID3:
+      restart_advertising_id(3);
+      return false;
+    case ADV_ID4:
+      restart_advertising_id(4);
+      return false;
+    case DEL_ID0:
+      delete_bond_id(0);
+      return false;
+    case DEL_ID1:
+      delete_bond_id(1);
+      return false;
+    case DEL_ID2:
+      delete_bond_id(2);
+      return false;
+    case DEL_ID3:
+      delete_bond_id(3);
+      return false;
+    case BATT_LV:
+      sprintf(str, "%4dmV", get_vcc());
+      send_string(str);
+      return false;
+    case ENT_DFU:
+      bootloader_jump();
+      return false;
+    }
+  }
+  else if (!record->event.pressed) {
+    switch (keycode) {
+    case ENT_SLP:
+      sleep_mode_enter();
+      return false;
+    }
+
+  }
+  return true;
+}
+;
